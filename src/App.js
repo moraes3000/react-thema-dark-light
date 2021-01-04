e613a1449a39b281d0bb4a4a6bda1771bdc73881
bruno <bruno_bmoraes@hotmail.com>
2021-01-04 19:46:49 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+
+import Header from './components/Header'
+
+import GlobalStyles from './styles/global'
+
+import { ThemeProvider } from 'styled-components'
+
+import light from './styles/themes/light';
+import dark from './styles/themes/dark';
+
+import usePersistedState from './utils/usePersistedState'
 
 function App() {
+  const [theme, setTheme] = usePersistedState('theme', light);
+
+  const toggleTheme = () => {
+    setTheme(theme.title === 'light' ? dark : light);
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Header toggleTheme={toggleTheme} />
+
+      <GlobalStyles />
+    </ThemeProvider>
   );
 }
 
